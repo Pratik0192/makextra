2b8a2d76ca8fd1ff76bb042084acc3d7945c3082
Pratik0192 <pratikchakraborty019@gmail.com>
2025-02-19 16:44:33 +0530

    banner frontend integration done

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,23 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import hero_1 from "../assets/hero_1.webp";
 import hero_2 from "../assets/hero_2.webp";
 import hero_3 from "../assets/hero_3.webp";
 import hero_4 from "../assets/hero_4.webp";
 import hero_5 from "../assets/hero_5.webp";
+import { ShopContext } from "../context/ShopContext";
+import axios from "axios";
 
-const images = [hero_1, hero_2, hero_3, hero_4, hero_5];
+
+
+//const images = [hero_1, hero_2, hero_3, hero_4, hero_5];
 
 const Hero = () => {
+  const { backendUrl } = useContext(ShopContext);
+  const [images, setImages] = useState([])
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const fetchBannerImage = async() => {
+    try {
+      const response = await axios.get(backendUrl + '/api/banner/all');
+      setImages(response.data.banners)
+    } catch (error) {
+      console.error("Error fetching banners:", error);
+    }
+  } 
+
   useEffect(() => {
+    fetchBannerImage();
+  }, [])
+  
+  useEffect(() => {
+    if(images.length === 0) return;
+
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 3000); // Auto-slide every 3 seconds
 
     return () => clearInterval(interval);
-  }, []);
+  }, [images.length]);
 
   const handlePrev = () => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
@@ -37,7 +58,7 @@ const Hero = () => {
         {images.map((image, index) => (
           <img
             key={index}
-            src={image}
+            src={image.imageUrl}
             alt={`Slide ${index}`}
             className="w-full h-auto flex-shrink-0"
           />
